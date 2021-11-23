4e8aba678df5dd9f6e1be0d6d6bf70a4e937119e
chris8736 <christophertam@brandeis.edu>
2021-11-23 18:27:16 -0500

    Update XGB2.py

--- src/XGB2.py
+++ src/XGB2.py
@@ -11,8 +11,8 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import GridSearchCV
 from preprocess import Preprocess
 
-# 5K-CV AUC-mu 99.33
-# Test AUC 93.79
+# 5K-CV AUC-mu 99.34
+# Test AUC 94.22
 
 # load data set from csv (converted from online xlsx)
 train_ppc = Preprocess(
@@ -24,8 +24,7 @@ train_y = train_ppc.output
 xgb = XGBClassifier(n_estimators=60, eval_metric='logloss', scale_pos_weight='5.2', learning_rate=0.3, reg_lambda=0,
                     use_label_encoder=False)
 
-param_grid = {'eval_metric': ['logloss', 'auc',
-                              'aucpr'], 'learning_rate': [.2, .25, .3, .35, .4]}
+param_grid = {'n_estimators': [102]}
 clf = GridSearchCV(xgb, param_grid, scoring='roc_auc')
 clf.fit(train_X, train_y)
 print(clf.best_params_)
