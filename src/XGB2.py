3f0a8792be77c6c383317422d9059ef56f646934
chris8736 <christophertam@brandeis.edu>
2021-11-23 18:01:06 -0500

    Add XGB2 with GridSearch

--- src/XGB2.py
+++ src/XGB2.py
@@ -8,10 +8,11 @@ from matplotlib import pyplot
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import plot_confusion_matrix
 from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
 from preprocess import Preprocess
 
-# 5K-CV AUC-mu 99.31
-# Test AUC 94.10
+# 5K-CV AUC-mu 99.33
+# Test AUC 93.79
 
 # load data set from csv (converted from online xlsx)
 train_ppc = Preprocess(
@@ -20,8 +21,14 @@ train_ppc = Preprocess(
 train_X = train_ppc.df
 train_y = train_ppc.output
 
-clf = XGBClassifier(n_estimators=60, eval_metric='logloss', scale_pos_weight=5.2,
+xgb = XGBClassifier(n_estimators=60, eval_metric='logloss', scale_pos_weight='5.2', learning_rate=0.3, reg_lambda=0,
                     use_label_encoder=False)
+
+param_grid = {'eval_metric': ['logloss', 'auc',
+                              'aucpr'], 'learning_rate': [.2, .25, .3, .35, .4]}
+clf = GridSearchCV(xgb, param_grid, scoring='roc_auc')
+clf.fit(train_X, train_y)
+print(clf.best_params_)
 
 print("Cross-validation 5-fold AUC scores:")
 scores = cross_val_score(clf, train_X, train_y, cv=5, scoring='roc_auc')
@@ -29,10 +36,11 @@ print(scores)
 print("Average: ", sum(scores) / len(scores))
 
 # Test set
-clf.fit(train_X, train_y)
 
 test_ppc = Preprocess(
     "../data/testing_stratified_20.csv").code_output().drop_output().onehot_categorical()
+
+clf.fit(train_X, train_y)
 
 test_X = test_ppc.df.to_numpy()
 test_y = test_ppc.output.to_numpy()
@@ -42,17 +50,3 @@ print("Testing AUC: ", metrics.roc_auc_score(
 
 plot_confusion_matrix(clf, test_X, test_y, values_format='d')
 plt.show()
-
-"""
-importance = permutation_importance(
-    clf, train_X, train_y, n_repeats=10, random_state=0)
-feature_names = [f"feature {i}" for i in range(train_X.shape[1])]
-importances = pd.Series(importance.importances_mean,
-                        index=feature_names).sort_values(ascending=False)
-fig, ax = plt.subplots()
-importances.plot.bar(yerr=importance.importances_std, ax=ax)
-ax.set_title("Feature importances using permutation on full model")
-ax.set_ylabel("Mean accuracy decrease")
-fig.tight_layout()
-plt.show()
-"""
